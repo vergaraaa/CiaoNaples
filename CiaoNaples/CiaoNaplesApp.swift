62291c995e23cfbcaff53d80999f086a8f363f9f
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-17 16:59:14 +0200

    Initial Commit

---
+++ CiaoNaples/CiaoNaplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiaoNaplesApp.swift
+//  CiaoNaples
+//
+//  Created by Edgar Ernesto Vergara Montiel on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CiaoNaplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CiaoNaples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CiaoNaples
+//
+//  Created by Edgar Ernesto Vergara Montiel on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
