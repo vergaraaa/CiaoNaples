6eeb99da9c7ded2b543c962d5f8da618b8e44d70
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-18 19:43:48 +0200

    feat: category enum

---
+++ CiaoNaples/Models/Location.swift
@@ -0,0 +1,19 @@
+//
+//  Location.swift
+//  CiaoNaples
+//
+//  Created by Edgar Ernesto Vergara Montiel on 18/10/23.
+//
+
+import Foundation
+
+struct Location {
+    let name: String
+    let coverImage: String
+    let images: [String]
+    let description: String
+    let zone: String
+    let recommendations: [String]
+    let latitude: Double
+    let longitude: Double
+}
