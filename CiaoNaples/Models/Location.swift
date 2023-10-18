d2410f60ddec67aa75f618aee93082701cb33813
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-18 19:51:37 +0200

    location

--- CiaoNaples/Models/Location.swift
+++ CiaoNaples/Models/Location.swift
@@ -6,14 +6,3 @@
 //
 
 import Foundation
-
-struct Location {
-    let name: String
-    let coverImage: String
-    let images: [String]
-    let description: String
-    let zone: String
-    let recommendations: [String]
-    let latitude: Double
-    let longitude: Double
-}
