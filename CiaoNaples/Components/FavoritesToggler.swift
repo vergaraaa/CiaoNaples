dc2dfafd0312a90abc203962bb85a153c4f105a5
Edgar Vergara <edgarvm13@hotmail.com>
2023-10-28 05:48:27 +0200

    refcator: removed animation

---
+++ CiaoNaples/Components/FavoritesToggler.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesToggler.swift
+//  CiaoNaples
+//
+//  Created by Edgar Ernesto Vergara Montiel on 28/10/23.
+//
+
+import SwiftUI
+
+struct FavoritesToggler: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesToggler()
+}
